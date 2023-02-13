4bf94ec91d3cd90b41d35cdeb7aa6fb795de26c5
ajaop <ajaopaul58@gmail.com>
2023-02-13 22:46:54 +0100

    Added gridview images

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,6 +1,8 @@
 import 'package:art_market/database_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -34,14 +36,14 @@ class _DashboardState extends State<Dashboard> {
         children: [
           Scaffold(
             body: SafeArea(
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 15.0,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20.0, 0, 30.0, 0),
-                    child: Row(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 0, 30.0, 0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    Row(
                       mainAxisSize: MainAxisSize.max,
                       children: [
                         CircleAvatar(
@@ -49,7 +51,7 @@ class _DashboardState extends State<Dashboard> {
                           backgroundColor: Color(0xffC9E4D0),
                           child: Text(
                             profileImageText,
-                            style: TextStyle(
+                            style: const TextStyle(
                                 color: Color(0xff1B3823),
                                 fontWeight: FontWeight.bold,
                                 fontSize: 23.0),
@@ -57,8 +59,57 @@ class _DashboardState extends State<Dashboard> {
                         ),
                       ],
                     ),
-                  )
-                ],
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Expanded(
+                        child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        topLeft: Radius.circular(10),
+                      ),
+                      child: RefreshIndicator(
+                        onRefresh: () {
+                          return getAllData();
+                        },
+                        child: GridView.custom(
+                          shrinkWrap: true,
+                          gridDelegate: SliverWovenGridDelegate.count(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 4,
+                            crossAxisSpacing: 4,
+                            pattern: [
+                              const WovenGridTile(1),
+                              const WovenGridTile(
+                                5 / 7,
+                                crossAxisRatio: 0.9,
+                                alignment: AlignmentDirectional.centerEnd,
+                              ),
+                            ],
+                          ),
+                          childrenDelegate:
+                              SliverChildBuilderDelegate((context, index) {
+                            return Container(
+                              decoration: const BoxDecoration(
+                                  color: Color(0xffC9E4D0),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15))),
+                              child: ClipRRect(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(15)),
+                                child: FadeInImage.memoryNetwork(
+                                  placeholder: kTransparentImage,
+                                  image: imageList[index],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          }, childCount: imageList.length),
+                        ),
+                      ),
+                    ))
+                  ],
+                ),
               ),
             ),
           ),
@@ -85,4 +136,20 @@ class _DashboardState extends State<Dashboard> {
       _loading = false;
     });
   }
+
+  List<String> imageList = [
+    'https://cdn.pixabay.com/photo/2019/03/15/09/49/girl-4056684_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/12/15/16/25/clock-5834193__340.jpg',
+    'https://cdn.pixabay.com/photo/2020/09/18/19/31/laptop-5582775_960_720.jpg',
+    'https://media.istockphoto.com/photos/woman-kayaking-in-fjord-in-norway-picture-id1059380230?b=1&k=6&m=1059380230&s=170667a&w=0&h=kA_A_XrhZJjw2bo5jIJ7089-VktFK0h0I4OWDqaac0c=',
+    'https://cdn.pixabay.com/photo/2019/11/05/00/53/cellular-4602489_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2017/02/12/10/29/christmas-2059698_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/01/29/17/09/snowboard-4803050_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/02/06/20/01/university-library-4825366_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/11/22/17/28/cat-5767334_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/12/13/16/22/snow-5828736_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/12/09/09/27/women-5816861_960_720.jpg',
+  ];
+
+  Future<void> getAllData() async {}
 }
