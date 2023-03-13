bb52ed63a50c662e3049f97e3445773963ab4cf6
ajaop <ajaopaul58@gmail.com>
2023-03-13 23:25:24 +0100

    Working on the Cart Page

--- lib/cart.dart
+++ lib/cart.dart
@@ -19,6 +19,7 @@ class _CartState extends State<Cart> {
   List<ArtItems>? retrievedItemsList;
   DatabaseService databaseService = DatabaseService();
   bool _loading = false;
+  int quantity = 1;
 
   @override
   void initState() {
@@ -93,10 +94,11 @@ class _CartState extends State<Cart> {
                                   itemCount: retrievedItemsList?.length ?? 0,
                                   itemBuilder: (context, position) {
                                     return CartItems(
-                                      position: position,
-                                      retrievedArtItems: retrievedItemsList,
-                                      getValues: getDefaultValues,
-                                    );
+                                        position: position,
+                                        quantityValue: quantity,
+                                        retrievedArtItems: retrievedItemsList,
+                                        qtyOperations: QtyOperations,
+                                        deleteItem: deleteItem);
                                   },
                                   separatorBuilder: (context, index) =>
                                       SizedBox(
@@ -141,19 +143,50 @@ class _CartState extends State<Cart> {
       _loading = false;
     });
   }
+
+  Future<void> deleteItem(itemId) async {
+    setState(() {
+      _loading = true;
+    });
+
+    await databaseService.removeItemFromCart(itemId, _messangerKey);
+    getDefaultValues();
+
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  void QtyOperations(bool isIncrease) {
+    if (isIncrease) {
+      if (quantity >= 1) {
+        setState(() {
+          quantity = quantity + 1;
+        });
+      }
+    } else {
+      if (quantity > 1) {
+        setState(() {
+          quantity = quantity - 1;
+        });
+      }
+    }
+  }
 }
 
 class CartItems extends StatelessWidget {
   const CartItems(
       {Key? key,
       required this.position,
+      required this.quantityValue,
       required this.retrievedArtItems,
-      this.getValues})
+      this.qtyOperations,
+      this.deleteItem})
       : super(key: key);
 
-  final int position;
+  final int position, quantityValue;
   final List<ArtItems>? retrievedArtItems;
-  final getValues;
+  final qtyOperations, deleteItem;
 
   @override
   Widget build(BuildContext context) {
@@ -161,77 +194,119 @@ class CartItems extends StatelessWidget {
 
     artPrice = NumberFormat.currency(locale: "en_NG", symbol: "₦")
         .format(retrievedArtItems?[position].artPrice);
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) =>
-                  ItemDetails(retrievedArtItem: retrievedArtItems![position]),
-            )).then((value) {
-          getValues();
-        });
-      },
-      child: Container(
-        decoration: const BoxDecoration(
-            //color: Color(0xffEEF7F0),
-            borderRadius: BorderRadius.all(Radius.circular(15))),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              height: 100.0,
-              width: 120.0,
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(15))),
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(15.0)),
-                child: FadeInImage.memoryNetwork(
-                  placeholder: kTransparentImage,
-                  image: retrievedArtItems![position].imageUrl,
-                  fit: BoxFit.cover,
+
+    DatabaseService databaseService = DatabaseService();
+
+    return Container(
+      decoration: const BoxDecoration(
+          //color: Color(0xffEEF7F0),
+          borderRadius: BorderRadius.all(Radius.circular(15))),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 100.0,
+            width: 120.0,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: retrievedArtItems![position].imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 30.0,
+          ),
+          Flexible(
+            child: Column(
+              children: [
+                Text(
+                  retrievedArtItems![position].artName,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 20.0),
                 ),
-              ),
+                const SizedBox(
+                  height: 7.0,
+                ),
+                Text(
+                  artPrice,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w600, fontSize: 17.0),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        qtyOperations(true);
+                      },
+                      child: const CircleAvatar(
+                          backgroundColor: Color(0xffEEF7F0),
+                          child: Text(
+                            '+',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 20.0),
+                          )),
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Text(
+                      quantityValue.toString(),
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        qtyOperations(false);
+                      },
+                      child: const CircleAvatar(
+                          backgroundColor: Color(0xffEEF7F0),
+                          child: Text(
+                            '-',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 20.0),
+                          )),
+                    ),
+                  ],
+                )
+              ],
             ),
-            const SizedBox(
-              width: 30.0,
-            ),
-            Flexible(
-              child: Column(
-                children: [
-                  Text(
-                    retrievedArtItems![position].artName,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 20.0),
-                  ),
-                  const SizedBox(
-                    height: 7.0,
-                  ),
-                  Text(
-                    artPrice,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w600, fontSize: 17.0),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
+          ),
+          SizedBox(
+            width: 30.0,
+          ),
+          InkWell(
+            onTap: () {
+              deleteItem(retrievedArtItems![position].docId);
+            },
+            child: Container(
+              height: 100.0,
               width: 50.0,
+              child: CircleAvatar(
+                  backgroundColor: Colors.grey[300],
+                  child: Icon(
+                    Icons.delete_outline,
+                    color: Colors.black,
+                    size: 27.0,
+                  )),
             ),
-            CircleAvatar(
-                backgroundColor: Colors.grey[300],
-                child: Icon(
-                  Icons.delete_outline,
-                  color: Colors.black,
-                  size: 27.0,
-                )),
-            SizedBox(
-              width: 10.0,
-            )
-          ],
-        ),
+          ),
+          SizedBox(
+            width: 10.0,
+          )
+        ],
       ),
     );
   }
