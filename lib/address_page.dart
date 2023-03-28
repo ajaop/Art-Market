99229ae98ed81a94906bf540bbf74bcddde8e384
ajaop <ajaopaul58@gmail.com>
2023-03-28 22:38:52 +0100

    today

--- lib/address_page.dart
+++ lib/address_page.dart
@@ -8,11 +8,82 @@ class AddressPage extends StatefulWidget {
   State<AddressPage> createState() => _AddressPageState();
 }
 
-const List<String> list = <String>['Nigeria', 'Zimbabwe', 'Ghana', 'Togo'];
+const List<String> list = <String>[
+  'Nigeria',
+  'Zimbabwe',
+  'Ghana',
+  'South Africa'
+];
+var countryState = {
+  'Nigeria': ['Lagos', 'Ogun', 'Oyo', 'Akwa Ibom'],
+  'Zimbabwe': ['Bulawayo', 'Harare', 'Manicaland', 'Mashonaland Central'],
+  'Ghana': ['Ahafo', 'Ashanti', 'Bono East', 'Brong Ahafo'],
+  'South Africa': ['Eastern Cape', 'Gauteng', 'Limpopo', 'Mpumalanga']
+};
+
+var nigeriaCity = {
+  'Lagos': ['Lagos', 'Ikeja', 'Lekki', 'Okorodu'],
+  'Ogun': ['Abokuta', 'Ijebu', 'Odeda', 'Sagamu'],
+  'Oyo': ['Ibadan', 'Egbeda', 'Iseyin', 'Iyanaofa'],
+  'Akwa Ibom': ['Uyo', 'Abak', 'Ikot Abasi', 'Nsit-Ibom']
+};
+
+var zimbabweCity = {
+  'Bulawayo': ['Barbourfields', 'Barham Green', 'Beacon Hill', 'Bellevue'],
+  'Harare': ['Harare', 'Chitungwiza', 'Epworth', 'Ruwa'],
+  'Manicaland': [
+    'Buhera District',
+    'Chimanimani District',
+    'Makoni District',
+    'Mutasa District'
+  ],
+  'Mashonaland Central': ['Bindura', 'Mbire', 'Guruve', 'Rushinga']
+};
+
+var ghanaState = {
+  'Ahafo': ['Goaso', 'Bechem', 'Duayaw Nkwanta', 'Techimantia'],
+  'Ashanti': [
+    'Adansi North',
+    'Adansi Asokwa',
+    'Adansi South',
+    'Afigya Kwabre North'
+  ],
+  'Bono East': [
+    'Atebubu-Amanten',
+    'Kintampo North',
+    'Kintampo South',
+    'Nkoranza North'
+  ],
+  'Greater Accra': [
+    'Accra Metropolitan',
+    'Ada East',
+    'Ada West',
+    'Adenta Municipal'
+  ]
+};
+
+var southAfricaCity = {
+  'Eastern Cape': ['Mthatha ', 'Komani', 'Makhanda', 'Dikeni'],
+  'Gauteng': ['Johannesburg', 'Boksburg', 'Alberton', 'Carletonville'],
+  'Limpopo': [
+    'Capricorn District',
+    'Mopani Districs',
+    'Sekhukhune District',
+    'Vhembe District'
+  ],
+  'Mpumalanga': [
+    'Ehlanzeni District',
+    'Gert Sibande District',
+    'Nkangala District'
+  ]
+};
 
 class _AddressPageState extends State<AddressPage> {
   int _index = 0;
-  String dropdownValue = list.first;
+  String? countryDropdownValue = list.first,
+      stateDropdownValue,
+      cityDropdownValue = list.first;
+  String countryName = "'" + "Nigeria" + "'";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -69,52 +140,127 @@ class _AddressPageState extends State<AddressPage> {
                               ),
                             ),
                             const SizedBox(
-                              height: 7.0,
+                              height: 5.0,
                             ),
-                            Container(
-                              height: 65.0,
-                              child: FormField(
-                                  builder: (FormFieldState<String> state) {
-                                return InputDecorator(
-                                  decoration: InputDecoration(
-                                    enabledBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      borderSide: BorderSide(
-                                          width: 1.5, color: Color(0xff1B3823)),
-                                    ),
-                                    hintText: 'Nigeria',
-                                  ),
+                            DropdownButtonFormField<String>(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                hintText: 'Niger',
+                              ),
+                              value: countryDropdownValue,
+                              onChanged: (String? value) {
+                                setState(() {
+                                  countryDropdownValue = value!;
+                                  countryName =
+                                      "'" + countryDropdownValue! + "'";
+                                  print(countryName);
+                                });
+                              },
+                              items: list.map<DropdownMenuItem<String>>(
+                                  (String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
                                   child: Padding(
                                     padding: const EdgeInsets.fromLTRB(
-                                        30.0, 0, 0.0, 0),
-                                    child: DropdownButtonHideUnderline(
-                                      child: DropdownButton<String>(
-                                        value: dropdownValue,
-                                        borderRadius:
-                                            BorderRadius.circular(12.0),
-                                        style: const TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 16.0),
-                                        onChanged: (String? value) {
-                                          setState(() {
-                                            dropdownValue = value!;
-                                          });
-                                        },
-                                        items: list
-                                            .map<DropdownMenuItem<String>>(
-                                                (String value) {
-                                          return DropdownMenuItem<String>(
-                                            value: value,
-                                            child: Text(
-                                              value,
-                                            ),
-                                          );
-                                        }).toList(),
-                                      ),
+                                        10.0, 0, 0, 0),
+                                    child: Text(
+                                      value,
+                                      style: TextStyle(color: Colors.black),
                                     ),
                                   ),
                                 );
-                              }),
+                              }).toList(),
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Text(
+                                'State',
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xff1B3823)),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5.0,
+                            ),
+                            DropdownButtonFormField<String>(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                hintText: 'Lagos',
+                              ),
+                              value: stateDropdownValue,
+                              onChanged: (String? value) {
+                                setState(() {
+                                  stateDropdownValue = value!;
+                                });
+                              },
+                              items: countryState[countryName]
+                                  ?.map<DropdownMenuItem<String>>(
+                                      (String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        10.0, 0, 0, 0),
+                                    child: Text(
+                                      value,
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Text(
+                                'City',
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xff1B3823)),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5.0,
+                            ),
+                            DropdownButtonFormField<String>(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                hintText: 'Lagos',
+                              ),
+                              value: cityDropdownValue,
+                              onChanged: (String? value) {
+                                setState(() {
+                                  cityDropdownValue = value!;
+                                });
+                              },
+                              items: list.map<DropdownMenuItem<String>>(
+                                  (String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        10.0, 0, 0, 0),
+                                    child: Text(
+                                      value,
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
                             )
                           ],
                         )),
